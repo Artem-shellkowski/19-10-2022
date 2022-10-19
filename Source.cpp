05ed6778abe8b16ecafc38ad6911c7cfff3e9378
Artem-shellkowski <114939650+Artem-shellkowski@users.noreply.github.com>
2022-10-19 18:32:21 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,65 @@
+//N 1
+/*
+#include<iostream>
+using namespace std;
+int main (){
+	int s = 0;
+	int x;
+	for (int i = 1; i <= 10; i++) {
+		cin >> x;
+		s += x;
+		
+}
+	cout << s;                                                          
+}
+*/
+/*
+//N 2
+#include<iostream>
+using namespace std;
+int main() {
+	int s = 0;
+	int i = 1;
+	int x;
+	while ( i <= 10) {
+		cin >> x;
+		s = s * i;
+
+	}
+	cout << s;
+}
+*/
+//N 3
+/*
+#include<iostream>
+using namespace std;
+int main (){
+	int s = 0;
+	int x;
+	for (int i = 1; i <= 10; i++) {
+		cin >> x;
+		s += x;
+
+}
+int s = s / 10
+	cout << s ;
+}
+*/
+//N 4
+
+#include<iostream>
+using namespace std;
+int main (){
+	int s = 0;
+	int i = 1;
+	int n;
+	int x;
+	cin >> n;
+	while ( i <= n ) {
+		cout << (int)x << endl;
+		s += -(int)x;
+
+}
+	cout << s ;
+}
+
